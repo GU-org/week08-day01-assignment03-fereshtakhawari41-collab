7cae8ccf448ec0f20e342a9a01bac905addd94ea
Fereshta Khawari <fereshtakhawari41@gmail.com>
2026-03-03 14:04:55 +0430

    Solotion for week 08 day 01 assignment 03

--- Week08/Day01/Assignment03/index.js
+++ Week08/Day01/Assignment03/index.js
@@ -1,11 +1,11 @@
 function bmiCalc(w,h){
-    let bmi
+    let bmi = w / (h * h);
 
     return bmi;
 
 }
 
-var weight=prompt("Please enter you weight in kg");
-var height=prompt("Please enter your height in m");
-let bmi=bmiCalc(weight,height);
+var w=prompt("Please enter you weight in kg");
+var h=prompt("Please enter your height in m");
+let bmi=bmiCalc(w,h);
 alert("Your bmi is "+bmi);
